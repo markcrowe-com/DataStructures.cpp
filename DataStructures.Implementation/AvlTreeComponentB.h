0ae86abccdd75419f178143ed72051739f73cfa4
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2020-11-29 16:56:20 +0000

    Refactor Lecture Code to work with interfaces

--- DataStructures.Implementation/AvlTreeComponentB.h
+++ DataStructures.Implementation/AvlTreeComponentB.h
@@ -2,59 +2,307 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-using namespace std;
-
-typedef enum { LH, EH, RH } balance;
-
-template <class ItemType>struct  TreeNode
+#include "AvlTreeNodeComponentB.h"
+namespace DataStructures
 {
-	ItemType info;
-	ItemType root;
-	balance bf;
-	TreeNode* left;
-	TreeNode* right;
-};
-
-template <class ItemType>class TreeType
-{
-public:
-	void DeleteItem(ItemType item);
-	void InsertItem(ItemType item);
-	string ToString();
-private:
-	TreeNode<ItemType>* root;
-};
-
-
-template<class ItemType>void Insert(TreeNode<ItemType>*& tree, ItemType item, bool& taller);
-template<class T>string ToInOrderString(TreeNode<T>* tree);
-
-template<class ItemType>string TreeType<ItemType>::ToString()
-// Calls recursive function Delete to delete item from tree.
-{
-	return ToInOrderString<ItemType>(root);
-}
-
-
-/// 03 - AVL Part 2 File : Slide 17-19
-template <class ItemType>
-void TreeType<ItemType>::DeleteItem(ItemType item)
-// Calls recursive function Delete to delete item from tree.
-{
-	bool shorter;
-	Delete(root, item, shorter);
-}
-template <class ItemType>
-void Delete(TreeNode<ItemType>*& tree, ItemType item, bool& shorter)
-// Deletes item from tree.
-// Post:	item is not in tree.
-{
-	if(tree != NULL)
+	using namespace std;
+	template<class T>class AvlTreeComponentB : public AvlTree<T>
 	{
-		if(item < tree->info)
+	public:
+		AvlTreeComponentB();
+		~AvlTreeComponentB();
+	public:
+		void Clear();
+		bool Contains(T candidate) const;
+		void Insert(T value);
+		bool IsEmpty() const;
+		AvlTreeNode<T>* Nodes() const;
+		bool Remove(T value);
+		string ToString() const;
+	private:
+		AvlTreeNodeComponentB<T>* root;
+	private:
+		void DeleteItem(T item);
+		void InsertItem(T item);
+	};
+
+	//
+//	constructor/desturtor
+//
+	template<class T> AvlTreeComponentB<T>::AvlTreeComponentB()
+	{
+		root = NULL;
+	}
+	template<class T> AvlTreeComponentB<T>::~AvlTreeComponentB() { }
+	#pragma region Public Methods
+	#pragma endregion
+	template<class T> void AvlTreeComponentB<T>::Clear()
+	{
+		root = NULL;
+	}
+	template<class T> bool AvlTreeComponentB<T>::Contains(T candidate) const
+	{
+		return ContainsValue<T>(root, candidate);
+	}
+
+	template<class T> bool AvlTreeComponentB<T>::IsEmpty() const
+	{
+		return root == NULL;
+	}
+	template<class T> void AvlTreeComponentB<T>::Insert(T value)
+	{
+		InsertItem(value);
+		//root = InsertValue<T>(root, value);
+	}
+	template<class T> AvlTreeNode<T>* AvlTreeComponentB<T>::Nodes() const
+	{
+		return root; // should be a deep copy of Nodes
+	}
+	template<class T> bool AvlTreeComponentB<T>::Remove(T value)
+	{
+		bool present = Contains(value);
+		if(present)
+			DeleteItem(value);
+		return present;
+	}
+	template<class T> string AvlTreeComponentB<T>::ToString() const
+	{
+		return ToInOrderString<T>(root);
+	}
+
+
+	template<class T>void InsertNode(AvlTreeNodeComponentB<T>*& tree, T item, bool& taller);
+
+	/// 03 - AVL Part 2 File : Slide 17-19
+	template <class T>
+	void AvlTreeComponentB<T>::DeleteItem(T item)
+		// Calls recursive function Delete to delete item from tree.
+	{
+		bool shorter;
+		Delete(root, item, shorter);
+	}
+	template <class T>
+	void Delete(AvlTreeNodeComponentB<T>*& tree, T item, bool& shorter)
+		// Deletes item from tree.
+		// Post:	item is not in tree.
+	{
+		if(tree != NULL)
 		{
-			Delete(tree->left, item, shorter);
-			// Look in left subtree.
+			if(item < tree->Value)
+			{
+				Delete(tree->leftTree, item, shorter);
+				// Look in left subtree.
+				if(shorter)
+					switch(tree->bf)
+					{
+						case LH: tree->bf = EH; break;
+						case EH: tree->bf = RH; shorter = false;
+							break;
+						case RH: DelRightBalance(tree, shorter);
+					}
+			}
+			else if(item > tree->Value)
+			{
+				Delete(tree->rightTree, item, shorter);
+				// Look in right subtree.
+				if(shorter)
+					switch(tree->bf)
+					{
+						case LH: DelLeftBalance(tree, shorter);
+							break;
+						case EH: tree->bf = LH; shorter = false; 							break;
+						case RH: tree->bf = EH; break;
+					}
+			}
+			else
+				DeleteNode(tree, shorter);
+			// Node found; call DeleteNode.
+		}
+		else
+		{
+			cerr << "\nNOTE: " << item << " not in the tree so cannot be deleted.";
+		}
+	}
+	/// 03 - AVL Part 2 File : Slide 17-19
+
+	/// 04 - LeftBalance File
+	template <class T>
+	void LeftBalance(AvlTreeNodeComponentB<T>*& tree, bool& taller)
+	{
+		AvlTreeNodeComponentB<T>* ls = tree->leftTree;
+		AvlTreeNodeComponentB<T>* rs;
+
+		switch(ls->bf)
+		{
+			case LH:	tree->bf = ls->bf = EH;
+				RotateRight(tree);
+				taller = false;
+				break;
+			case EH:	cerr << "Tree already balanced " << endl;
+				break;
+			case RH:	rs = ls->rightTree;
+				switch(rs->bf)
+				{
+					case LH:	tree->bf = RH;
+						ls->bf = EH;		break;
+					case EH:	tree->bf = ls->bf = EH;	break;
+					case RH:	tree->bf = EH;
+						ls->bf = LH;		break;
+				}
+				rs->bf = EH;
+				RotateLeft(tree->leftTree);
+				RotateRight(tree);
+				taller = false;
+		}
+	}
+	/// 04 - LeftBalance File
+
+
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 29-36
+	template<class T>void AvlTreeComponentB<T> ::InsertItem(T item)
+		// Calls recursive function Insert to insert item into tree.
+	{
+		bool taller = false;
+		InsertNode(root, item, taller);
+	}
+	template <class T>
+	void InsertNode(AvlTreeNodeComponentB<T>*& tree, T item, bool& taller)
+		// Inserts item into tree.				  // Post:	item is in tree; search property is maintained.
+	{
+		if(tree == NULL)
+		{	// Insertion place found.
+			tree = new AvlTreeNodeComponentB<T>(item);
+			taller = true;
+		}
+		else if(item == tree->Value)
+			cerr << "Duplicate key is not allowed in AVL tree." << endl;
+		else if(item < tree->Value)
+		{
+			InsertNode(tree->leftTree, item, taller);  		// Insert into left subtree
+			if(taller)
+				switch(tree->bf)
+				{
+					case LH: LeftBalance(tree, taller); break;
+					case EH: tree->bf = LH; break;
+					case RH: tree->bf = EH;
+						taller = false; break;
+				}
+		}
+		else {
+			InsertNode(tree->rightTree, item, taller);   		// Insert into right subtree
+			if(taller)
+				switch(tree->bf)
+				{
+					case RH: RightBalance(tree, taller); break;
+					case EH: tree->bf = RH; break;
+					case LH: tree->bf = EH;
+						taller = false; break;
+				}
+		}
+	}
+	template <class T>
+	void RightBalance(AvlTreeNodeComponentB<T>*& tree, bool& taller)
+	{
+		AvlTreeNodeComponentB<T>* rs = tree->rightTree;
+		AvlTreeNodeComponentB<T>* ls;
+
+		switch(rs->bf)
+		{
+			case RH:	tree->bf = rs->bf = EH;
+				RotateLeft(tree);
+				taller = false;
+				break;
+
+			case EH:	cerr << "Tree already balanced " << endl;
+				break;
+
+
+			case LH:	ls = rs->leftTree;
+				switch(ls->bf)
+				{
+					case RH:	tree->bf = LH;
+						rs->bf = EH;
+						break;
+					case EH:	tree->bf = rs->bf = EH;
+						break;
+					case LH:	tree->bf = EH;
+						rs->bf = RH;
+						break;
+				}
+				ls->bf = EH;
+				RotateRight(tree->rightTree);
+				RotateLeft(tree);
+				taller = false;
+		}
+	}
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 29-36
+
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 15/16
+	template<class T>void RotateLeft(AvlTreeNodeComponentB<T>*& tree)
+	{
+		AvlTreeNodeComponentB<T>* rs;
+		if(tree == NULL)
+			cerr << "It is impossible to rotate an empty tree in RotateLeft"
+			<< endl;
+		else if(tree->RightTree == NULL)
+			cerr << "It is impossible to make an empty subtree the root in RotateLeft" << endl;
+		else
+		{
+			rs = tree->rightTree;
+			tree->rightTree = rs->leftTree;
+			rs->leftTree = tree;
+			tree = rs;
+		}
+	}
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 15/16
+
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 21/22
+	template<class T>void RotateRight(AvlTreeNodeComponentB<T>*& tree)
+	{
+		AvlTreeNodeComponentB<T>* ls;
+		if(tree == NULL)
+			cerr << "It is impossible to rotate an empty tree in RotateRight"
+			<< endl;
+		else if(tree->LeftTree == NULL)
+			cerr << "It is impossible to make an empty subtree the root in RotateRight" << endl;
+		else
+		{
+			ls = tree->leftTree;
+			tree->leftTree = ls->rightTree;
+			ls->rightTree = tree;
+			tree = ls;
+		}
+	}
+	/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 21/22
+
+	/// 06 - Delete Source Code File : Slide 4-7
+	template <class T>
+	void DeleteNode(AvlTreeNodeComponentB <T>*& tree, bool& shorter)
+		// Delete the node pointed to by tree.
+		// Post: The user's data in the node pointed to by tree is no longer in the tree. // If tree is a leaf node or has only one non-NULL child pointer, the node
+		// pointed to by tree is deleted; otherwise, the user's data is replaced by its
+		// logical predecessor and the predecessor's node is deleted.
+	{
+		T data;	AvlTreeNodeComponentB <T>* tempPtr;
+		tempPtr = tree;
+		if(tree->LeftTree == NULL)
+		{
+			tree = tree->rightTree;
+			delete tempPtr;
+			shorter = true;
+		}
+		else if(tree->RightTree == NULL)
+		{
+			tree = tree->leftTree;
+			delete tempPtr;
+			shorter = true;
+		}
+		else
+		{
+			GetPredecessor(tree, data);
+			tree->Value = data;
+			Delete(tree->leftTree, data, shorter);
+			// Delete the predecessor node
 			if(shorter)
 				switch(tree->bf)
 				{
@@ -64,342 +312,78 @@ void Delete(TreeNode<ItemType>*& tree, ItemType item, bool& shorter)
 					case RH: DelRightBalance(tree, shorter);
 				}
 		}
-		else if(item > tree->info)
+	}
+	template <class T>
+	void GetPredecessor(AvlTreeNodeComponentB<T>* tree, T& data)
+		// Sets data to the info member of the right-most node in tree.
+	{
+		tree = tree->leftTree;
+		while(tree->RightTree != NULL)
+			tree = tree->rightTree;
+		data = tree->Value;
+	}
+	template <class T>
+	void DelRightBalance(AvlTreeNodeComponentB<T>*& tree, bool& shorter)
+	{
+		AvlTreeNodeComponentB<T>* rs = tree->rightTree;
+		AvlTreeNodeComponentB<T>* ls;
+		switch(rs->bf)
 		{
-			Delete(tree->right, item, shorter);
-			// Look in right subtree.
-			if(shorter)
-				switch(tree->bf)
+			case RH:	tree->bf = rs->bf = EH;
+				RotateLeft(tree);
+				shorter = true; break;
+			case EH:	tree->bf = RH;
+				rs->bf = LH;
+				RotateLeft(tree);
+				shorter = false; break;
+			case LH:	ls = rs->leftTree;
+				switch(ls->bf)
 				{
-					case LH: DelLeftBalance(tree, shorter);
+					case RH:	tree->bf = LH;
+						rs->bf = EH; break;
+					case EH:	tree->bf = rs->bf = EH;
 						break;
-					case EH: tree->bf = LH; shorter = false; 							break;
-					case RH: tree->bf = EH; break;
-				}
+					case LH:	tree->bf = EH;
+						rs->bf = RH; break;
+				} // END SWITCH
+
+				ls->bf = EH;
+				RotateRight(tree->rightTree);
+				RotateLeft(tree);
+				shorter = true;
 		}
-		else
-			DeleteNode(tree, shorter);
-		// Node found; call DeleteNode.
 	}
-	else
+	template <class T>
+	void DelLeftBalance(AvlTreeNodeComponentB<T>*& tree, bool& shorter)
 	{
-		cout << "\nNOTE: " << item << " not in the tree so cannot be 					deleted.";
+		AvlTreeNodeComponentB<T>* ls = tree->leftTree;
+		AvlTreeNodeComponentB<T>* rs;
+		switch(ls->bf)
+		{
+			case LH:	tree->bf = ls->bf = EH;
+				RotateRight(tree);
+				shorter = true; break;
+			case EH:	tree->bf = LH;
+				ls->bf = RH;
+				RotateRight(tree);
+				shorter = false; break;
+			case RH:	rs = ls->rightTree;
+				switch(rs->bf)
+				{
+					case LH:	tree->bf = RH;
+						ls->bf = EH; break;
+					case EH:	tree->bf = ls->bf = EH;
+						break;
+					case RH:	tree->bf = EH;
+						ls->bf = LH; break;
+				} // END SWITCH
+				rs->bf = EH;
+				RotateLeft(tree->leftTree);
+				RotateRight(tree);
+				shorter = true;
+		}
 	}
+	/// 06 - Delete Source Code File : Slide 4-7
+
+	/// ToString
 }
-/// 03 - AVL Part 2 File : Slide 17-19
-
-/// 04 - LeftBalance File
-template <class ItemType>
-void LeftBalance(TreeNode<ItemType>*& tree, bool& taller)
-{
-	TreeNode<ItemType>* ls = tree->left;
-	TreeNode<ItemType>* rs;
-
-	switch(ls->bf)
-	{
-		case LH:	tree->bf = ls->bf = EH;
-			RotateRight(tree);
-			taller = false;
-			break;
-		case EH:	cerr << "Tree already balanced " << endl;
-			break;
-		case RH:	rs = ls->right;
-			switch(rs->bf)
-			{
-				case LH:	tree->bf = RH;
-					ls->bf = EH;		break;
-				case EH:	tree->bf = ls->bf = EH;	break;
-				case RH:	tree->bf = EH;
-					ls->bf = LH;		break;
-			}
-			rs->bf = EH;
-			RotateLeft(tree->left);
-			RotateRight(tree);
-			taller = false;
-	}
-}
-/// 04 - LeftBalance File
-
-
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 29-36
-template<class ItemType>void TreeType<ItemType> ::InsertItem(ItemType item)
-// Calls recursive function Insert to insert item into tree.
-{
-	bool taller = false;
-	Insert(root, item, taller);
-}
-template <class ItemType>
-void Insert(TreeNode<ItemType>*& tree, ItemType item, bool& taller)
-// Inserts item into tree.				  // Post:	item is in tree; search property is maintained.
-{
-	if(tree == NULL)
-	{	// Insertion place found.
-		tree = new TreeNode<ItemType>;
-		tree->left = NULL;
-		tree->right = NULL;
-		tree->info = item;
-		tree->bf = EH;
-		taller = true;
-	}
-	else if(item == tree->info)
-		cerr << "Duplicate key is not allowed in AVL tree." << endl;
-	else if(item < tree->info)
-	{
-		Insert(tree->left, item, taller);  		// Insert into left subtree
-		if(taller)
-			switch(tree->bf)
-			{
-				case LH: LeftBalance(tree, taller); break;
-				case EH: tree->bf = LH; break;
-				case RH: tree->bf = EH;
-					taller = false; break;
-			}
-	}
-	else {
-		Insert(tree->right, item, taller);   		// Insert into right subtree
-		if(taller)
-			switch(tree->bf)
-			{
-				case RH: RightBalance(tree, taller); break;
-				case EH: tree->bf = RH; break;
-				case LH: tree->bf = EH;
-					taller = false; break;
-			}
-	}
-}
-template <class ItemType>
-void RightBalance(TreeNode<ItemType>*& tree, bool& taller)
-{
-	TreeNode<ItemType>* rs = tree->right;
-	TreeNode<ItemType>* ls;
-
-	switch(rs->bf)
-	{
-		case RH:	tree->bf = rs->bf = EH;
-			RotateLeft(tree);
-			taller = false;
-			break;
-
-		case EH:	cerr << "Tree already balanced " << endl;
-			break;
-
-
-		case LH:	ls = rs->left;
-			switch(ls->bf)
-			{
-				case RH:	tree->bf = LH;
-					rs->bf = EH;
-					break;
-				case EH:	tree->bf = rs->bf = EH;
-					break;
-				case LH:	tree->bf = EH;
-					rs->bf = RH;
-					break;
-			}
-			ls->bf = EH;
-			RotateRight(tree->right);
-			RotateLeft(tree);
-			taller = false;
-	}
-}
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 29-36
-
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 15/16
-template<class ItemType>void RotateLeft(TreeNode<ItemType>*& tree)
-{
-	TreeNode<ItemType>* rs;
-	if(tree == NULL)
-		cerr << "It is impossible to rotate an empty tree in RotateLeft"
-		<< endl;
-	else if(tree->right == NULL)
-		cerr << "It is impossible to make an empty subtree the root in RotateLeft" << endl;
-	else
-	{
-		rs = tree->right;
-		tree->right = rs->left;
-		rs->left = tree;
-		tree = rs;
-	}
-}
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 15/16
-
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 21/22
-template<class ItemType>void RotateRight(TreeNode<ItemType>*& tree)
-{
-	TreeNode<ItemType>* ls;
-	if(tree == NULL)
-		cerr << "It is impossible to rotate an empty tree in RotateRight"
-		<< endl;
-	else if(tree->left == NULL)
-		cerr << "It is impossible to make an empty subtree the root in RotateRight" << endl;
-	else
-	{
-		ls = tree->left;
-		tree->left = ls->right;
-		ls->right = tree;
-		tree = ls;
-	}
-}
-/// 02 - Lecture 2 - AVL Trees (Part 1) File: Slide 21/22
-
-/// 06 - Delete Source Code File : Slide 4-7
-template <class ItemType>
-void DeleteNode(TreeNode <ItemType>*& tree, bool& shorter)
-// Delete the node pointed to by tree.
-// Post: The user's data in the node pointed to by tree is no longer in the tree. // If tree is a leaf node or has only one non-NULL child pointer, the node
-// pointed to by tree is deleted; otherwise, the user's data is replaced by its
-// logical predecessor and the predecessor's node is deleted.
-{
-	ItemType data;	TreeNode <ItemType>* tempPtr;
-	tempPtr = tree;
-	if(tree->left == NULL)
-	{
-		tree = tree->right;
-		delete tempPtr;
-		shorter = true;
-	}
-	else if(tree->right == NULL)
-	{
-		tree = tree->left;
-		delete tempPtr;
-		shorter = true;
-	}
-	else
-	{
-		GetPredecessor(tree, data);
-		tree->info = data;
-		Delete(tree->left, data, shorter);
-		// Delete the predecessor node
-		if(shorter)
-			switch(tree->bf)
-			{
-				case LH: tree->bf = EH; break;
-				case EH: tree->bf = RH; shorter = false;
-					break;
-				case RH: DelRightBalance(tree, shorter);
-			}
-	}
-}
-template <class ItemType>
-void GetPredecessor(TreeNode<ItemType>* tree, ItemType& data)
-// Sets data to the info member of the right-most node in tree.
-{
-	tree = tree->left;
-	while(tree->right != NULL)
-		tree = tree->right;
-	data = tree->info;
-}
-template <class ItemType>
-void DelRightBalance(TreeNode<ItemType>*& tree, bool& shorter)
-{
-	TreeNode<ItemType>* rs = tree->right;
-	TreeNode<ItemType>* ls;
-	switch(rs->bf)
-	{
-		case RH:	tree->bf = rs->bf = EH;
-			RotateLeft(tree);
-			shorter = true; break;
-		case EH:	tree->bf = RH;
-			rs->bf = LH;
-			RotateLeft(tree);
-			shorter = false; break;
-		case LH:	ls = rs->left;
-			switch(ls->bf)
-			{
-				case RH:	tree->bf = LH;
-					rs->bf = EH; break;
-				case EH:	tree->bf = rs->bf = EH;
-					break;
-				case LH:	tree->bf = EH;
-					rs->bf = RH; break;
-			} // END SWITCH
-
-			ls->bf = EH;
-			RotateRight(tree->right);
-			RotateLeft(tree);
-			shorter = true;
-	}
-}
-template <class ItemType>
-void DelLeftBalance(TreeNode<ItemType>*& tree, bool& shorter)
-{
-	TreeNode<ItemType>* ls = tree->left;
-	TreeNode<ItemType>* rs;
-	switch(ls->bf)
-	{
-		case LH:	tree->bf = ls->bf = EH;
-			RotateRight(tree);
-			shorter = true; break;
-		case EH:	tree->bf = LH;
-			ls->bf = RH;
-			RotateRight(tree);
-			shorter = false; break;
-		case RH:	rs = ls->right;
-			switch(rs->bf)
-			{
-				case LH:	tree->bf = RH;
-					ls->bf = EH; break;
-				case EH:	tree->bf = ls->bf = EH;
-					break;
-				case RH:	tree->bf = EH;
-					ls->bf = LH; break;
-			} // END SWITCH
-			rs->bf = EH;
-			RotateLeft(tree->left);
-			RotateRight(tree);
-			shorter = true;
-	}
-}
-/// 06 - Delete Source Code File : Slide 4-7
-
-/// ToString
-
-template<class T>string TtoString(T const& t)
-{
-	ostringstream myObjectStream;
-	myObjectStream << t;
-	return myObjectStream.str();
-}
-static string BalanceFactorToString(balance height)
-{
-	if(height == LH)
-		return "Left Unbalanced";
-	else if(height == RH)
-		return "Right Unbalanced";
-	else //if (height == Balanced)
-		return "Balanced";
-}
-
-template<class T>string ToString(TreeNode<T>* tree)
-{
-	string lhs = tree->left == NULL ? "NULL" : TtoString(tree->left->info);
-	string rhs = tree->right == NULL ? "NULL" : TtoString(tree->right->info);
-	string balanceFactor = BalanceFactorToString(tree->bf);
-
-	return "\n\t " + TtoString(tree->info) + ":\tLeft: " + lhs + " \tRight: " + rhs + " \tBalance Factor: " + balanceFactor;
-}
-template<class T>string ToInOrderString(TreeNode<T>* tree)
-{
-	if(tree == NULL)
-		return "";
-	else
-	{
-		string text = ToInOrderString(tree->left);
-		text += ToString(tree);
-		text += ToInOrderString(tree->right);
-		return text;
-	}
-}
-template<class T>string ToPreOrderString(TreeNode<T>* tree)
-{
-	if(tree != NULL)
-	{
-		string text = TtoString(tree->info) + " ";
-		text += ToPreOrderString(tree->left);
-		text += ToPreOrderString(tree->right);
-		return text;
-	}
-	else
-		return "";
-}
-
---
+++ DataStructures.Implementation/AvlTreeNodeComponentB.h
@@ -0,0 +1,123 @@
+#pragma once
+#include <iostream>
+#include "../DataStructures/AvlTreeNode.h"
+#include "../DataStructures/AvlBalanceFactor.h"
+
+namespace DataStructures
+{
+	using namespace std;
+	typedef enum { LH, EH, RH } balance;
+	template<class T> struct AvlTreeNodeComponentB : public AvlTreeNode<T>
+	{
+		AvlTreeNodeComponentB();
+		AvlTreeNodeComponentB(T);
+	public:
+		#pragma region Methods : get const
+		#pragma endregion
+		int getHeight() const;
+		AvlTreeNode<T>* getLeftTree() const;
+		AvlTreeNode<T>* getRightTree() const;
+		T getValue() const;
+		#pragma region Methods : set
+		#pragma endregion
+		void setHeight(int);
+		void setLeftTree(AvlTreeNode<T>*);
+		void setRightTree(AvlTreeNode<T>*);
+		void setValue(T);
+		#pragma region Methods
+		#pragma endregion
+		int CalculateHeightDifference() const;
+		bool IsComplete() const;
+		bool IsLeaf() const;
+		bool IsNotComplete() const;
+		void UpdateHeight();
+		#pragma region Properties
+		#pragma endregion
+		__declspec(property(get = getHeight, put = setHeight)) int Height;
+		__declspec(property(get = getLeftTree, put = setLeftTree)) AvlTreeNode<T>* LeftTree;
+		__declspec(property(get = getRightTree, put = setRightTree)) AvlTreeNode<T>* RightTree;
+		__declspec(property(get = getValue, put = setValue)) T Value;
+		#pragma region Fields
+		#pragma endregion
+		balance bf;
+		AvlTreeNodeComponentB<T>* leftTree;
+		AvlTreeNodeComponentB<T>* rightTree;
+	private:
+		T value;
+	};
+	template<class T> AvlTreeNodeComponentB<T>::AvlTreeNodeComponentB()
+	{
+		this->bf = EH;
+		this->leftTree = this->rightTree = NULL;
+	}
+	template<class T> AvlTreeNodeComponentB<T>::AvlTreeNodeComponentB(T value)
+	{
+		this->bf = EH;
+		this->leftTree = this->rightTree = NULL;
+		this->value = value;
+	}
+	#pragma region Methods : get const
+	#pragma endregion
+	template<class T> int AvlTreeNodeComponentB<T>::getHeight() const
+	{
+		cerr << "AvlTreeNodeComponentB<T>::getHeight()" << endl;
+		return 1;
+	}
+	template<class T> AvlTreeNode<T>* AvlTreeNodeComponentB<T>::getLeftTree() const
+	{
+		return this->leftTree;
+	}
+	template<class T> AvlTreeNode<T>* AvlTreeNodeComponentB<T>::getRightTree() const
+	{
+		return this->rightTree;
+	}
+	template<class T> T AvlTreeNodeComponentB<T>::getValue() const
+	{
+		return this->value;
+	}
+	#pragma region Methods : set
+	#pragma endregion
+	template<class T> void AvlTreeNodeComponentB<T>::setHeight(int value)
+	{
+		cerr << "AvlTreeNodeComponentB<T>::setHeight(int value)" << endl;
+	}
+	template<class T> void AvlTreeNodeComponentB<T>::setLeftTree(AvlTreeNode<T>* value)
+	{
+		//this->leftTree = value;
+	}
+	template<class T> void AvlTreeNodeComponentB<T>::setRightTree(AvlTreeNode<T>* value)
+	{
+		//this->rightTree = value;
+	}
+	template<class T> void AvlTreeNodeComponentB<T>::setValue(T value)
+	{
+		this->value = value;
+	}
+	#pragma region Methods
+	#pragma endregion
+	template<class T> int AvlTreeNodeComponentB<T>::CalculateHeightDifference() const
+	{
+		if(bf == balance::EH)
+			return AvlBalanceFactor::Balanced;
+		else if(bf == balance::LH)
+			return AvlBalanceFactor::LeftUnbalanced;
+		else
+			return AvlBalanceFactor::RightUnbalanced;
+	}
+	template<class T> bool AvlTreeNodeComponentB<T>::IsComplete() const
+	{
+		return this->leftTree != NULL && this->rightTree != NULL;
+	}
+	template<class T> bool AvlTreeNodeComponentB<T>::IsLeaf() const
+	{
+		return this->leftTree == NULL && this->rightTree == NULL;
+	}
+	template<class T> bool AvlTreeNodeComponentB<T>::IsNotComplete() const
+	{
+		return this->leftTree == NULL || this->rightTree == NULL;
+	}
+	template<class T> void AvlTreeNodeComponentB<T>::UpdateHeight()
+	{
+		cerr << "AvlTreeNodeComponentB<T>::UpdateHeight()" << endl;
+	}
+}
