e5c1921e05f1ee3d77fbcbf3fdc8622e4d59335b
Mark Crowe <66536097+markcrowe-com@users.noreply.github.com>
2021-02-18 00:43:14 +0000

    Clean 1st pass

--- DataStructures.Implementation/GraphType.h
+++ DataStructures.Implementation/GraphType.h
@@ -12,7 +12,7 @@ namespace DataStructures
 	{
 	public:
 		GraphType();// constructor, default of 50 vertices.
-		GraphType(int maxV);                         	// parameterized constructor, maxV <= 50.
+		GraphType(int maxV);// parameterized constructor, maxV <= 50.
 		~GraphType();
 
 		void MakeEmpty();
@@ -35,42 +35,26 @@ namespace DataStructures
 		int edges[50][50];
 		bool* marks;
 	};
-
-	template<class VertexType>
-	GraphType<VertexType>::GraphType()
-		// Post: Arrays of size 50 are dynamically allocated for
-		//       marks and vertices. numVertices is set to 0;
-		//       maxVertices is set to 50.
+	template<class VertexType>GraphType<VertexType>::GraphType()
 	{
 		numVertices = 0;
 		maxVertices = 50;
 		vertices = new VertexType[50];
 		marks = new bool[50];
 	}
-	template<class VertexType>
-	GraphType<VertexType>::GraphType(int maxV)
-		// Post: Arrays of size maxV are dynamically
-		// allocated for marks and vertices.
-		// numVertices is set to 0; maxVertices is set to maxV.
+	template<class VertexType>GraphType<VertexType>::GraphType(int maxV)
 	{
 		numVertices = 0;
 		maxVertices = maxV;
 		vertices = new VertexType[maxV];
 		marks = new bool[maxV];
 	}
-	template<class VertexType>
-	GraphType<VertexType>::~GraphType()
-		// Post: arrays for vertices and marks have been   // deallocated.
+	template<class VertexType>GraphType<VertexType>::~GraphType()
 	{
 		delete[] vertices;
 		delete[] marks;
 	}
-
-	template<class VertexType>
-	void GraphType<VertexType>::AddVertex(VertexType vertex)
-		// Post: vertex has been stored in vertices.
-		// Corresponding row and column of edges have been set to NULL_EDGE.
-		// numVertices has been incremented.
+	template<class VertexType>void GraphType<VertexType>::AddVertex(VertexType vertex)
 	{
 		vertices[numVertices] = vertex;
 		for(int index = 0; index < numVertices; index++)
@@ -80,11 +64,7 @@ namespace DataStructures
 		}
 		numVertices++;
 	}
-	template<class VertexType>
-	void GraphType<VertexType>::AddEdge(
-		VertexType fromVertex,
-		VertexType toVertex, int weight)
-		// Post: Edge (fromVertex, toVertex) is stored in edges.
+	template<class VertexType>void GraphType<VertexType>::AddEdge(VertexType fromVertex, VertexType toVertex, int weight)
 	{
 		int row;
 		int column;
@@ -92,12 +72,7 @@ namespace DataStructures
 		col = IndexIs(vertices, toVertex);
 		edges[row][col] = weight;
 	}
-	template<class VertexType>
-	int GraphType<VertexType>::WeightIs(
-		VertexType fromVertex,
-		VertexType toVertex)
-		// Post: Function value = weight associated with the
-		// 		edge (fromVertex, toVertex).
+	template<class VertexType>int GraphType<VertexType>::WeightIs(VertexType fromVertex, VertexType toVertex)
 	{
 		int row;
 		int column;
@@ -105,38 +80,24 @@ namespace DataStructures
 		col = IndexIs(vertices, toVertex);
 		return edges[row][col];
 	}
-
-
-	template<class VertexType>
-	int IndexIs(VertexType* vertices,
-		VertexType vertex)
-		// Post: Function value = index of vertex in vertices.
+	template<class VertexType>int IndexIs(VertexType* vertices, VertexType vertex)
 	{
 		int index = 0;
 		while(!(vertex == vertices[index]))
 			index++;
 		return index;
 	}
-	template<class VertexType>
-	int IndexIs(VertexType* vertices,
-		VertexType vertex)
-		// Post: Function value = index of vertex in vertices.
+	template<class VertexType>int IndexIs(VertexType* vertices, VertexType vertex)
 	{
 		int index = 0;
 		while(!(vertex == vertices[index]))
 			index++;
 		return index;
 	}
-	template<class VertexType>
-	void GraphType<VertexType>::GetToVertices(
-		VertexType vertex,
-		queue<VertexType>& adjvertexQ)
+	template<class VertexType>void GraphType<VertexType>::GetToVertices(VertexType vertex, queue<VertexType>& adjvertexQ)
 	{
-		int fromIndex;
-		int toIndex;
-
-		fromIndex = IndexIs(vertices, vertex);
-		for(toIndex = 0; toIndex < numVertices; toIndex++)
+		int fromIndex = IndexIs(vertices, vertex);
+		for(int toIndex = 0; toIndex < numVertices; toIndex++)
 			if(edges[fromIndex][toIndex] != NULL_EDGE)
 				adjvertexQ.Enqueue(vertices[toIndex]);
 	}
